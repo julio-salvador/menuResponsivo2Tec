ee3aa7e1809bfe04fd0701cf67a4c8b59e917a6c
Julio Salvador Girardi <julio.girardi@escola.pr.gov.br>
2026-08-17 11:27:35 +0000

    fazendo as mod

---
+++ script.js
@@ -0,0 +1,9 @@
+let fontTamanho = 16;
+const corpo = document.body;
+
+const aumentar = document.getElementById("aumentar");
+const aumentar = document.getElementById("diminuir");
+
+aumentar.addEventListener("click", function()){
+    
+}
